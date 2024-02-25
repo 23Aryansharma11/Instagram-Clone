6c015c884c8ed16dd92c7bed8856852cb7c72a60
Aryan Sharma <23aryansharma11@gmail.com>
2024-02-25 15:48:21 +0530

    feat(user): add login user controller

--- instagram-clone-backend/src/controllers/user.controller.js
+++ instagram-clone-backend/src/controllers/user.controller.js
@@ -7,7 +7,24 @@ import mongoose from "mongoose";
 import { uploadToCloud, deleteFromCloud } from "../Utils/Cloudinary.js";
 
 
-const registerUser = AsyncHandler(async (req, res)=>{
+const generateAccessTokenAndRefreshToken = async (user)=>{
+    try {
+        const accessToken = user.generateAccessToken();
+        const refreshToken = user.generateRefreshToken();
+        await user.save({validateBeforeSave:false}); 
+        return {accessToken, refreshToken};
+    } catch (error) {
+        console.log(error)
+        throw new ApiError(500,"Internal Server Error. Something went wrong while generating tokens");
+    }
+}
+
+const options = {
+    httpOnly: true,
+    secure: true
+}
+
+const register = AsyncHandler(async (req, res)=>{
     const {fullName, userName, dateOfBirth, email, password} = req.body;
 
     if([fullName, userName, email, password].some((field) => field?.trim() == ""))  throw new ApiError(406, "All fields are required");
@@ -46,5 +63,22 @@ const registerUser = AsyncHandler(async (req, res)=>{
         return res.status(201).json(new ApiResponse(201, createdUser,  "Account has been created successfully.") );
 })
 
+const login = AsyncHandler(async(req, res)=>{
+    const {userName, email, password} = req.body;
+    if(!userName && !email) throw new ApiError(400,  "Username or Email is required.");
 
-export {registerUser}
+    const user = User.findOne({$or: [{userName}, {email}]});
+    if(!user) throw new ApiError(404, "User not found.");
+
+    const isPasswordValid = await user.isPasswordCorrect(password);
+    if(!isPasswordValid) throw new ApiError(401, "Incorrect Password.");
+
+    const {accessToken,  refreshToken} = await generateAccessTokenAndRefreshToken(user);
+    // Remove password and token from output
+    user.password = undefined;
+    user.refreshToken = undefined;
+
+    return res.status(200).json(new ApiResponse(200, {user, accessToken, refreshToken}, "Login successfull")).cookie("accessToken: ", accessToken, options).cookie("refreshToken: ", refreshToken, options);
+
+})
+export {register, login}
--- instagram-clone-backend/src/routes/user.routes.js
+++ instagram-clone-backend/src/routes/user.routes.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import {upload} from "../middlewares/multer.middleware.js";
-import { registerUser } from "../controllers/user.controller.js";
+import { register, login } from "../controllers/user.controller.js";
 
 const router = Router();
 
 router.route("/register").post(
     upload.single("avatar"),  // multer middleware for handling the uploaded file (image)
-    registerUser            // controller function to handle user registration request
+    register            // controller function to handle user registration request
 );
-
+router.route("/login").post(login)
 
 export default router;
